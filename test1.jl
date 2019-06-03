4ba08a39f1d1caccfd368004de18da8adb82d8c8
Javier Yu <javiercyu@gmail.com>
2019-06-03 14:30:45 -0700

    Dynamics seem to be working

--- test1.jl
+++ test1.jl
@@ -19,22 +19,27 @@ end
 
 function veh_Alin(t, x, u)
     A = 0.754 # Vehicle Property - Surface Area
-    r = 0.4899 # Vehicle Property - vehicle radius, not given in paper
     m = 1.0 # Vehicle Property - vehicle quality, not given in paper
     g = 9.81 # Gravity
+    R = 6371000
+    dt = 1.0
 
+    r = R + x[3] 
     Cd = get_Cd(u)
     rho = dens(x[3])
     a11 = (-Cd * rho * A * x[1] / m)
     a12 = (-g * cos(x[2])) 
     a21 = (u[1] * rho * A / (2 * m) + ((1 / r) + (g / x[2]^2)) * cos(x[2]))
+    a23 = -x[1] * cos(x[2]) / r ^ 2
     a22 = (((g / x[1]) - (x[1] / r)) * cos(x[2]))
     a31 = (sin(x[2]))
     a32 = (x[1] * cos(x[2]))
 
     Amat = [a11 a12 0;
-            a21 a22 0;
+            a21 a22 a23;
             a31 a32 0]
+    
+    Amat = eye(3) + dt * Amat
 
     return Amat
 end
@@ -45,13 +50,14 @@ function veh_Blin(t, x, u)
     m = 1.0 # Vehicle Property - vehicle quality, not given in paper
     g = 9.81 # Gravity
     K = 1.11 # Vehicle Property
+    dt = 1.0
 
     rho = dens(x[3])
     B = [(K * rho * x[1]^2 * A * u[1] / m);
          (rho * x[1] * A / (2 * m));
          0]
     
-    return B
+    return dt * B
 end
 
 function dens(h)
@@ -68,18 +74,22 @@ end
 
 function vehf(t, x, u)
     A = 0.754 # Vehicle Property - Surface Area
-    r = 0.4899 # Vehicle Property - vehicle radius, not given in paper
     m = 1.0 # Vehicle Property - vehicle quality, not given in paper
     g = 9.81 # Gravity
+    dt = 1.0
+    R = 6371000
     # x = [v, th, h]
 
     xn = copy(x)
     rho = dens(x[3])
     Cd = get_Cd(u)
+    r = R + x[3] 
 
-    xn[1] += -(Cd * rho * x[1] ^ 2 * A) / 2 * m - g * sin(x[2])
-    xn[2] += (u[1] * rho * x[1] * A) / 2 * m + (x[1] / r - g / x[1]) * cos(x[2])
-    xn[3] += x[1] * sin(x[2])
+    @show(Cd)
+
+    xn[1] += dt * -(Cd * rho * x[1] ^ 2 * A) / 2 * m - g * sin(x[2])
+    xn[2] += dt * (u[1] * rho * x[1] * A) / 2 * m + (x[1] / r - g / x[1]) * cos(x[2])
+    xn[3] += dt * x[1] * sin(x[2])
     return xn
 end
 
@@ -88,9 +98,9 @@ function veh_test()
     x0 = [11000.0; -0.13962; 110000];
 
     Q = 1.0 * speye(3)
-    Q[2, 2] = 0.0 # Quadratic costs on velocity and height
-    Q[1, 1] = 10.0 # Penalize high velocities
-    Q[3, 3] = 1.0 # Penalize losing altitude
+    #Q[2, 2] = 0.0 # Quadratic costs on velocity and height
+    #Q[1, 1] = 10.0 # Penalize high velocities
+    #Q[3, 3] = 1.0 # Penalize losing altitude
     R = 0.0 * speye(1)
     P = 1e1 * Q
     N = 100
@@ -99,7 +109,7 @@ function veh_test()
     ub = [[-Clmax], [Clmax]]
     xb = nothing # we may need to include some terminal velocity constraints in this
 
-    (Xplan, Uplan) = scpMPC(vehf, veh_Alin, veh_Blin, Q, R, P, x0, N, ub=ub, xb=xb)
+    #(Xplan, Uplan) = scpMPC(vehf, veh_Alin, veh_Blin, Q, R, P, x0, N, ub=ub, xb=xb)
 
     x = x0
     Xactual = [x]
@@ -107,17 +117,18 @@ function veh_test()
     Xold = nothing
     Uold = nothing
     for i in 1:30
-        (X, U) = scpMPC(vehf, veh_Alin, veh_Blin, Q, R, P, x, N, ub=ub, xb=xb,
-                        Xguess=Xold, Uguess=Uold)
+        #(X, U) = scpMPC(vehf, veh_Alin, veh_Blin, Q, R, P, x, N, ub=ub, xb=xb,
+        #                Xguess=Xold, Uguess=Uold)
         #(X, U) = scpMPC(f, Alin, Blin, Q, R, P, x, N, ub=ub)
-        u = [U[1] + 0.1 * randn()]
+        #u = [U[1] + 0.1 * randn()]
+        u = 0.5
         push!(Uactual, u)
-        x = vehf(i, x, u) + 0.1 * randn(3)
+        x = vehf(i, x, u) #+ 0.01 * randn(3)
         push!(Xactual, x)
         println(i)
     
-        Xold = [X[4:end]; zeros(3)]
-        Uold = [U[2:end]; zeros(1)]
+        #Xold = [X[4:end]; zeros(3)]
+        #Uold = [U[2:end]; zeros(1)]
     end
     
     Xactual = vcat(Xactual...)
@@ -125,13 +136,14 @@ function veh_test()
    
     figure(1)
     clf()
-    plot(Xactual[1:2:end], label="\$x_{1,a}\$", color="red")
-    plot(Xactual[2:2:end], label="\$x_{2,a}\$", color="blue")
+    plot(Xactual[1:3:end], label="\$x_{1,a}\$", color="red")
+    plot(Xactual[2:3:end], label="\$x_{2,a}\$", color="blue")
+    plot(Xactual[3:3:end], label="\$x_{3,a}\$", color="green")
     plot(Uactual, label="\$u_a\$", color="black")
     
-    plot(Xplan[1:2:30*2], label="\$x_{1,a}\$", color="red", linestyle="--")
-    plot(Xplan[2:2:30*2], label="\$x_{2,a}\$", color="blue", linestyle="--")
-    plot(Uplan[1:30], label="\$u_a\$", color="black", linestyle="--")
+    #plot(Xplan[1:2:30*2], label="\$x_{1,a}\$", color="red", linestyle="--")
+    #plot(Xplan[2:2:30*2], label="\$x_{2,a}\$", color="blue", linestyle="--")
+    #plot(Uplan[1:30], label="\$u_a\$", color="black", linestyle="--")
     legend()
     return
 end
@@ -201,6 +213,7 @@ function simulate(x0)
     x = f(i, x, u) + 0.1 * randn(2)
     push!(Xactual, x)
     println(i)
+    
 
     Xold = [X[3:end]; zeros(2)]
     Uold = [U[2:end]; zeros(1)]
@@ -213,6 +226,7 @@ function simulate(x0)
   clf()
   plot(Xactual[1:2:end], label="\$x_{1,a}\$", color="red")
   plot(Xactual[2:2:end], label="\$x_{2,a}\$", color="blue")
+  
   plot(Uactual, label="\$u_a\$", color="black")
 
   plot(Xplan[1:2:30*2], label="\$x_{1,a}\$", color="red", linestyle="--")
