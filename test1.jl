b9435e80d9c589febdf8a3648e35c16dd49e1809
Javier Yu <javiercyu@gmail.com>
2019-06-03 09:15:42 -0700

    starting the simulation stuff.

--- test1.jl
+++ test1.jl
@@ -9,9 +9,25 @@ function Blin(t, x, u)
   return [0.0; 1.0]
 end
 
+function simulate(Alin, Blin, A, B, Q, R, P, x0, N, ub)
+    # This is the first step no guess
+    (X, U) = scpMPC(Alin, Blin, Q, R, P, x0, N, ub=ub)
+
+    fc() = scpMPC(Alin, Blin, Q, R, P, x0, N, ub=ub, Xguess=X, Uguess=U)
+
+    # at each step:
+    # x0 = current state
+    # solve the scpMPC
+    # propogate dynamics with u and noise
+    # shift the X,U for next guess 
+    # 
+
+    return 
+end
+
 function main()
   x0 = [3.0; 3.0]
-  A = [1.0 1.0;
+  A = [1.0 0.1;
        0.0 1.0]
   B = [0.0;
        1.0]
@@ -19,12 +35,18 @@ function main()
   R = 1.0 * speye(1)
   P = 1e1 * Q
   N = 100
-  ub = [[-1.0], [1.0]]
+  ub = [[-0.05], [0.05]]
   #ub = nothing
 
   fa() = linMPC(A, B, Q, R, P, x0, N, ub=ub)
   #@btime $fa()
   @time fa()
+  (X, U) = linMPC(A, B, Q, R, P, x0, N, ub=ub)
+  clf()
+  plot(X[1:2:end])
+  plot(X[2:2:end])
+  plot(U)
+  return
 
   fb() = scpMPC(Alin, Blin, Q, R, P, x0, N, ub=ub)
   #@btime $fb()
@@ -34,6 +56,8 @@ function main()
   fc() = scpMPC(Alin, Blin, Q, R, P, x0, N, ub=ub, Xguess=X, Uguess=U)
   #@btime $fc()
   @time fc()
+
+
 
   clf()
   plot(X[1:2:end])
